fdc091ee04810e11045fdc2b50225717d39c5070
Jordan Moore <46928924+jordanmoore753@users.noreply.github.com>
2019-05-17 17:33:11 -0400

    Upload Debugging

---
+++ Exercises/Debugging/fixcoordinates.rb
@@ -0,0 +1,46 @@
+class Person
+  attr_reader :name
+  attr_accessor :location
+
+  def initialize(name)
+    @name = name
+  end
+
+  def teleport_to(latitude, longitude)
+    @location = GeoLocation.new(latitude, longitude)
+  end
+end
+
+class GeoLocation
+  attr_reader :latitude, :longitude
+
+  def initialize(latitude, longitude)
+    @latitude = latitude
+    @longitude = longitude
+  end
+
+  def to_s
+    "(#{latitude}, #{longitude})"
+  end
+
+  def ==(other)
+    to_s == other.to_s
+  end
+end
+
+# Example
+
+ada = Person.new('Ada')
+ada.location = GeoLocation.new(53.477, -2.236)
+
+grace = Person.new('Grace')
+grace.location = GeoLocation.new(-33.89, 151.277)
+
+ada.teleport_to(-33.89, 151.277)
+
+puts ada.location                   # (-33.89, 151.277)
+puts grace.location                 # (-33.89, 151.277)
+puts ada.location == grace.location # expected: true
+                                    # actual: false
+
+                                    # The problem is that location is a collaborator object, meaning that we need to compare the value of the instance variables of the object. We're originally checking for equality of the object id's, not their location values.
--- Exercises/Medium 1/highestlowestcards.rb
+++ Exercises/Medium 1/highestlowestcards.rb
@@ -1,34 +1,18 @@
-class Card
-  attr_reader :rank, :suit
-
-  @@deck = []
-
-  def initialize(rank, suit)
-    @rank = rank
-    @suit = suit
-    @@deck << [rank, suit]
-  end
-
-  def convert_royal(n)
-    royals = ['Jack', 'Queen', 'King']
-
-    case n 
-    when 'Ace' then 11
-    when royals.include?(n) then 10
-    else
-      n
-    end
-  end
-
-  def to_s
-    "#{rank} of #{suit}"
+module CanSwim
+  def swim
+    'swim'
   end
 end
 
+module CanTrain
+  def train
+    'train'
+  end
+end
 
+class Dog
+  include CanSwim
+  include CanTrain
+end
 
-cards = [Card.new(2, 'Hearts'),
-         Card.new(10, 'Diamonds'),
-         Card.new('Ace', 'Clubs')]
-
-puts cards
+p Dog.ancestors

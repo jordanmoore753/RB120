b4e89aa50c25d31812aed76101d02d43b05890ab
Jordan Moore <46928924+jordanmoore753@users.noreply.github.com>
2019-05-22 07:31:51 -0400

    Update doesitrock.rb

--- Exercises/Debugging/doesitrock.rb
+++ Exercises/Debugging/doesitrock.rb
@@ -1,26 +1,59 @@
-class Person
-  attr_reader :name, :shoes, :foot_size
-  
-  def initialize(name, foot_size)
-    @name = name
-    @foot_size = foot_size
-    @shoes = []
+class AuthenticationError < StandardError; end
+
+# A mock search engine
+# that returns a random number instead of an actual count.
+class SearchEngine
+  def self.count(query, api_key)
+    unless valid?(api_key)
+      raise AuthenticationError, 'API key is not valid.'
+    end
+
+    rand(200_000)
   end
-  
-  def buy_shoes(brand)
-    @shoes << Shoes.new(brand, foot_size)
+
+  private
+
+  def self.valid?(key)
+    key == 'LS1A'
   end
 end
 
-class Shoes
-  attr_reader :size, :brand
-  
-  def initialize(brand, size)
-    @brand = brand
-    @size = size
+module DoesItRock
+  API_KEY = 'LS1'
+
+  class NoScore; end
+
+  class Score
+    def self.for_term(term)
+      positive = SearchEngine.count(%{"#{term} rocks"}, API_KEY).to_f
+      negative = SearchEngine.count(%{"#{term} is not fun"}, API_KEY).to_f
+
+      positive / (positive + negative)
+    rescue StandardError
+      obj = NoScore.new
+    end
+  end
+
+  def self.find_out(term)
+    score = Score.for_term(term)
+
+    case score
+    when NoScore
+      "No idea about #{term}..."
+    when 0...0.5
+      "#{term} is not fun."
+    when 0.5
+      "#{term} seems to be ok..."
+    else
+      "#{term} rocks!"
+    end
+  rescue StandardError => e 
+    e.message
   end
 end
 
-me = Person.new('Jordan', 9.5)
-me.buy_shoes('Adidas')
-p me
+# Example (your output may differ)
+
+puts DoesItRock.find_out('Sushi')       # Sushi seems to be ok...
+puts DoesItRock.find_out('Rain')        # Rain is not fun.
+puts DoesItRock.find_out('Bug hunting') # Bug hunting rocks!
